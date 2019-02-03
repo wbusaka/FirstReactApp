0995881c258c823c69b7e3d2aa2eefa370eb1d3a
Wilson Busaka <wilbusaka@gmail.com>
2019-02-03 17:45:57 +0000

    added CardList.js file

--- src/robots.js
+++ src/robots.js
@@ -21,6 +21,42 @@ export const robots=[
         id:4,
         name:'Patricia Lebsack',
         username: 'Karianne',
-        email: 'Julliane,oconnor@kory.org'
+        email: 'Julliane.oconnor@kory.org'
+    },
+    {
+        id:5,
+        name:'Chelsea Dietrich',
+        username: 'Kamren',
+        email: 'Lucio_heitinga@annie.ca'
+    },
+    {
+        id:6,
+        name:'Mrs. Denis Shulist',
+        username: 'LEopold_Cokary',
+        email: 'Karlech_Dach@jasper.org'
+    },
+    {
+        id:7,
+        name:'Kurtis Weinsnut',
+        username: 'Elywyn.skiles',
+        email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+        id:8,
+        name:'Nicholas Lebsack',
+        username: 'Niko',
+        email: 'nick.carey@kory.biz'
+    },
+    {
+        id:9,
+        name:'Denilson Aernie',
+        username: 'Deno',
+        email: 'denis.elis@kory.org'
+    },
+    {
+        id:10,
+        name:'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julliane.oconnor@kory.org'
     }
 ]
