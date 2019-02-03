d33b00c65d0555b681a84fee273812d0230a6ed9
Wilson Busaka <wilbusaka@gmail.com>
2019-02-03 17:06:24 +0000

    first commit

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots=[
+    {
+        id:1,
+        name:'Leanne Graham',
+        username: 'Bret',
+        email: 'sincere@april.biz'
+    },
+    {
+        id:2,
+        name:'Ervin Howell',
+        username:'Antonette',
+        email: 'shannah@melissa.tv'
+    },
+    {
+        id:3,
+        name:'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yenesia.net'
+    },
+    {
+        id:4,
+        name:'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julliane,oconnor@kory.org'
+    }
+]
